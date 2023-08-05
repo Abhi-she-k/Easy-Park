2864717870b07622bd02d781fd425c58b09c4283
Abhishek Paul <abhishek.paul0705@gmail.com>
2023-08-04 23:18:22 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import { React, useState, useMemo} from "react";
 import './App.css';
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import {usePosition} from 'use-position';
+
+
+
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyAQvx5oeGKYXE2PMOwXko-Qa4-Ge40XrpA",
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
+
+  const [searchInput, setSearchInput] = useState(""); 
+
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const search = (event) => {
+    event.preventDefault();
+    console.log(searchInput);
+  };
+
+
+
+  if (!isLoaded) return <div><h1>Loading...</h1></div>;
   return ( 
     
   <div className='Home'>
     <Map/> 
     <div className="Menu">
       
-      <span><h1 id="Title">EasyPark</h1> </span>      
-      <span><input id="Search"></input> </span>    
-      <span><button>Create Listing</button> </span>    
-      <span><button>Login</button> </span>  
+      <span id="Header">
+        <h1>EasyPark</h1>
+      </span>      
+      <span className="Wrapper">
+      <form onSubmit={search}>
+        <input id="Search" type="text" placeholder="Toronto, ON, Canada" value={searchInput} onChange={handleSearchChange}/>
+        <button id = "Search-btn" type="submit"  >Go</button> 
+      </form>
+      </span>
+         
+      <span><button id="btn">Post Spot</button> </span>    
+      <span><button id="btn">Login</button> </span>  
 
     </div>
   </div>
 );
 }
 
-
-const mapOptions = {
-  mapTypeControl : false,
-  streetViewControl:false,
-  fullscreenControl:false,
-}
-
 function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const { latitude, longitude, error } = usePosition();
+
+  const center = {
+    lat: latitude ,
+    lng: longitude ,
+  };
+
+  const mapOptions = {
+    mapTypeControl : false,
+    streetViewControl:false,
+    fullscreenControl:false,
+  }
 
   return (
     <GoogleMap 
@@ -44,3 +74,5 @@ function Map() {
     </GoogleMap>
   );
 }
+
+
